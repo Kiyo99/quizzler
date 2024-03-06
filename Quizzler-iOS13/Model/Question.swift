ca272eca2a84ec0a673498d1823c7015ed8cad1f
kiyo99 <kiofavour@gmail.com>
2024-03-06 14:34:06 -0500

    finished Quizzler. Implemented MVC

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,22 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Godsfavour Ngo Kio on 2024-03-06.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct Question{
+    let text: String
+    let answer: [String]
+    let correctAnswer: String
+    
+    init(q: String, a: [String], correctAnswer: String) {
+        self.text = q
+        self.answer = a
+        self.correctAnswer = correctAnswer
+    }
+}
